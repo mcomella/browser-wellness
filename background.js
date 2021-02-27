5b18cb40a1ec67627b6856b4b4fd7f99bb158800
Michael Comella <michael.l.comella@gmail.com>
2021-02-26 17:51:29 -0800

    Can unpause whole extension.

--- background.js
+++ background.js
@@ -1,31 +1,39 @@
+const UNPAUSE_MILLIS = 15 /* min */ * 60 /* sec */ * 1000 /* millis */;
+
 const pauseUrl = browser.runtime.getURL('pause.html');
 
 const pausedTLDs = [
-    'foxnews.com',
-    'nytimes.com',
-    'economist.com'
 ];
 
-let unpausedTLDs = {
-    
-};
+var isAddonPaused = true;
 
 function maybeRedirectRequest(request) {
-    if (request.type != 'main_frame') {
+    if (!isAddonPaused ||
+            request.type != 'main_frame') {
         return;
     }
 
-    console.log(request);
-    let domain = new URL(request.url).host;
-    console.log(domain);
-    let isDomainPaused = pausedTLDs.find(paused => domain.endsWith(paused)).length > 0;
+    const domain = new URL(request.url).host;
+    const isDomainPaused = !!pausedTLDs.find(paused => domain.endsWith(paused));
     if (!isDomainPaused) {
         return;
     }
 
-    return {redirectUrl: pauseUrl};
+    const encodedRequestUrl = encodeURI(request.url);
+    const redirectUrl = pauseUrl + '?request=' + encodedRequestUrl;
+    return {redirectUrl: redirectUrl};
 }
 
+function onMessage(message, sender, sendResponse) {
+    console.log('message ' + message);
+    // todo: validate it's the right message.
+
+    // todo: pause only specific uris.
+    isAddonPaused = false;
+    setTimeout(e => isAddonPaused = true, UNPAUSE_MILLIS);
+}
+
+browser.runtime.onMessage.addListener(onMessage);
 browser.webRequest.onBeforeRequest.addListener(
     maybeRedirectRequest,
     {urls: ["<all_urls>"]}, // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Match_patterns
