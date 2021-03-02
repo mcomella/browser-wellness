93ecc6fdb01ce4d5ab267fbd4815e57a8d824e8d
Michael Comella <michael.l.comella@gmail.com>
2021-03-01 19:05:06 -0800

    add ability to save sites.

---
+++ options/options.js
@@ -0,0 +1,29 @@
+const KEY_PAUSED_SITES = 'paused-sites';
+
+const pausedSitesTextarea = document.getElementById('paused-sites');
+const saveButton = document.getElementById('save');
+
+const storage = browser.storage.local;
+
+function restoreOptions() {
+    const toGet = {};
+    toGet[KEY_PAUSED_SITES] = true;
+
+    storage.get(toGet).then(keys => {
+        pausedSitesTextarea.value = keys[KEY_PAUSED_SITES];
+    }, error => {
+        console.error('unable to fetch options: ' + error);
+        textarea.innerText = 'DON\'T CLICK SAVE.\nERROR: ' + error;
+    });
+}
+
+function saveSites(e) {
+    e.preventDefault();
+
+    const toSet = {};
+    toSet[KEY_PAUSED_SITES] = pausedSitesTextarea.value;
+    storage.set(toSet);
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+saveButton.addEventListener('click', saveSites);
