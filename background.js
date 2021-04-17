a667377f4b3ff1c1290df25a5737a7a79cf61b82
Michael Comella <michael.l.comella@gmail.com>
2021-04-17 11:32:16 -0700

    Specify pause duration per page.

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
-const UNPAUSE_MINS = 15;
-const UNPAUSE_MILLIS = UNPAUSE_MINS * 60 /* sec */ * 1000 /* millis */;
-
 const pauseUrl = browser.runtime.getURL('pause.html'); // cached just in case many requests are made.
 
 var userPausedSites = new Map(); // set later.
@@ -11,7 +8,7 @@ function maybeRedirectRequest(request) {
     }
 
     const userPausedSite = getUserPausedSiteFromUrl(request.url);
-    const isSitePaused = userPausedSite && !userPausedSite.isUnpaused;
+    const isSitePaused = userPausedSite && userPausedSite.unpauseUntil < new Date();
     if (!isSitePaused) {
         return;
     }
@@ -22,29 +19,31 @@ function maybeRedirectRequest(request) {
     return {redirectUrl: redirectUrl};
 }
 
-function unpauseSite(site) {
-    console.debug(`unpausing site for ${UNPAUSE_MINS} minutes: ${site}`);
-    const userPausedSite = getUserPausedSiteFromUrl(site);
+function unpauseSite(siteObj) {
+    const url = siteObj.url;
+    const userPausedSite = getUserPausedSiteFromUrl(url);
     if (!userPausedSite) {
-        console.error(`Unexpectedly unable to unpause, site not found: ${site}`);
+        console.error(`Unexpectedly unable to unpause, site not found: ${url}`);
         return;
     }
 
-    userPausedSite.isUnpaused = true;
-    setTimeout(e => {
-        console.debug(`pausing site: ${site}`);
-        userPausedSite.isUnpaused = false;
-    }, UNPAUSE_MILLIS);
+    const unpauseMin = siteObj.unpauseMin;
+    const unpauseMillis = unpauseMin * 60 /* seconds */ * 1000 /* millis */;
+
+    const now = new Date();
+    userPausedSite.unpauseUntil = new Date(now.getTime() + unpauseMillis);
+    console.debug(`unpausing site for ${unpauseMin} minutes until ${userPausedSite.unpauseUntil}: ${url}`);
 }
 
 function reloadSites() {
     browser.storage.local.get({pausedSites: true}).then(keys => {
         userPausedSites = new Map(); // reset data structure.
 
+        // TODO: keys.pausedSites.split not a function if not loaded from disk.
         const sitesOnDisk = keys.pausedSites.split('\n');
         sitesOnDisk.forEach(site => {
             userPausedSites.set(site, {
-                isUnpaused: false,
+                unpauseUntil: new Date(),
             });
         });
         console.debug('Paused TLDs loaded');
